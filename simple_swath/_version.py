97ece91a5924c2508aad14bdef4061f5d49abe87
robertxa <xavier.robert@univ-grenoble-alpes.fr>
2024-10-15 15:28:47 +0200

    Correct simple_swath

--- simple_swath/_version.py
+++ simple_swath/_version.py
@@ -1,4 +1,4 @@
 # Copyright (c) 2024 Xavier Robert <xavier.robert@ird.fr> and Benjamin Lehmann <lehmann.benj@gmail.com>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-__version__ = "2.0.5"
+__version__ = "2.0.6"
